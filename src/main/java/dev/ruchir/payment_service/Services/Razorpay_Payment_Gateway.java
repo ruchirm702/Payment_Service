7762f43398c602c7fcb5588ae04e4a7ddea857e9
Ruchir Mandora <ruchirm702@gmail.com>
2024-07-31 16:37:34 +0530

    Added RazorPay Dependemcy

--- src/main/java/dev/ruchir/payment_service/Services/Razorpay_Payment_Gateway.java
+++ src/main/java/dev/ruchir/payment_service/Services/Razorpay_Payment_Gateway.java
@@ -1,2 +1,13 @@
-package dev.ruchir.payment_service.Services;public class Razorpay_Payment_Gateway {
+package dev.ruchir.payment_service.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Razorpay_Payment_Gateway implements Payment_Service{
+
+    @Override
+    public String generate_Payment_Link(Long order_Id){
+        //Make a call to RazorPay to generate the Payment Link
+        return null;
+    }
 }
