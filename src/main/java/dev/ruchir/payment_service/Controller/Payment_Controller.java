1abd561f878b3ef9932d0058da57190dd97a77e7
Ruchir Mandora <ruchirm702@gmail.com>
2024-07-31 14:26:33 +0530

    Created generate_Payment_Link api

--- src/main/java/dev/ruchir/payment_service/Controller/Payment_Controller.java
+++ src/main/java/dev/ruchir/payment_service/Controller/Payment_Controller.java
@@ -2,6 +2,7 @@ package dev.ruchir.payment_service.Controller;
 
 
 import dev.ruchir.payment_service.DTOs.Generate_Payment_Link_Request_DTO;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Payment_Controller {
 
+    @PostMapping()
     public String generate_Payment_Link(@RequestBody Generate_Payment_Link_Request_DTO requestDto){
         return null;
     }
+
+
+
 }
