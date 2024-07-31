be66668cc6d6825de9189a932f4f06b277555141
Ruchir Mandora <ruchirm702@gmail.com>
2024-07-31 21:44:52 +0530

    Implemented Configuartion : Razorpay_Client_Config

---
+++ src/main/java/dev/ruchir/payment_service/Configs/Razorpay_Client_Config.java
@@ -0,0 +1,23 @@
+package dev.ruchir.payment_service.Configs;
+
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Razorpay_Client_Config{
+
+    @Value("${razorpay.key.id}")
+    private String razorpayKeyId;
+
+    @Value("${razorpay.key.secret}")
+    private String razorpayKeySecret;
+
+    @Bean
+    public RazorpayClient createRazorpayClient() throws RazorpayException {
+        return new RazorpayClient(razorpayKeyId, razorpayKeySecret);
+    }
+
+}
