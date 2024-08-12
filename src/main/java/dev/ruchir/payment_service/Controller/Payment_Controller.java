ac7aa20c61b7133438e0be362df72139a8fdcbda
Ruchir Mandora <ruchirm702@gmail.com>
2024-08-12 23:05:48 +0530

    Optimzation

--- src/main/java/dev/ruchir/payment_service/Controller/Payment_Controller.java
+++ src/main/java/dev/ruchir/payment_service/Controller/Payment_Controller.java
@@ -1,6 +1,5 @@
 package dev.ruchir.payment_service.Controller;
 
-
 import com.razorpay.RazorpayException;
 import com.stripe.exception.StripeException;
 import dev.ruchir.payment_service.DTOs.Generate_Payment_Link_Request_DTO;
@@ -13,24 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payments")
-
 public class Payment_Controller {
-    private Payment_Service payment_service;
 
-        public Payment_Controller(@Qualifier("stripePaymentGateway") Payment_Service payment_service) {
-            this.payment_service = payment_service;
-        }
+    private final Payment_Service payment_service;
 
+    public Payment_Controller(@Qualifier("stripePaymentGateway") Payment_Service payment_service) {
+        this.payment_service = payment_service;
+    }
 
-    //POST -> http://localhost:8080/payments
     @PostMapping()
     public String generate_Payment_Link(@RequestBody Generate_Payment_Link_Request_DTO requestDto) throws RazorpayException, StripeException {
-        //Ideally we should handle the exception in the controller using Controller Advice
         return payment_service.generate_Payment_Link(requestDto.getOrder_Id());
     }
 
     @PostMapping("/webhook")
-    public void handleWebhookEvent(@RequestBody Object object){
-            System.out.println("Webhook triggered");
+    public void handleWebhookEvent(@RequestBody Object object) {
+        // Placeholder for webhook event handling logic
+        System.out.println("Webhook triggered");
     }
 }
